3761ded657d3b8c6740b34f9ad1685480527dd74
jinseong-bae <67159239+jinseong-bae@users.noreply.github.com>
2020-08-25 01:05:03 +0900

    프론트엔드 추가
    - codepen을 활용해 프론트엔드 템플릿을 사용해서 붙였다.
    - css가 브라우저에서 자동으로 업데이트되지 않는 이유를 알았다. 브라우저에서 캐시를 계속 가지고 있기 때문. 이를 피하면서 계속 수정하기 위해 css 경로 뒤에 임의의 알파벳을 지속적으로 추가하며 수정하였다. 나중에 디자인이 확정되면 캐시를 지우고 깔끔하게 업로드할 예정.

--- price_hunter.py
+++ price_hunter.py
@@ -53,7 +53,7 @@ def price_hunting():
                 send_email(mail_address)
                 db.infos.update_one({'model': order_URL}, {'$set': {'flag': 1}})
             else:
-                db.infos.update_one({'model': order_URL}, {'$set': {'current_price': price_tag.text[4:9]}})
+                db.infos.update_one({'model': order_URL}, {'$set': {'current_price': price_tag.text[2:9]}})
 
         elif order_URL.startswith("https://www.fwrd"):
             price_tag = soup.select_one(
@@ -63,17 +63,17 @@ def price_hunting():
                 send_email(mail_address)
                 db.infos.update_one({'model': order_URL}, {'$set': {'flag': 1}})
             else:
-                db.infos.update_one({'model': order_URL}, {'$set': {'current_price': price_tag.text.lstrip('$')}})
+                db.infos.update_one({'model': order_URL}, {'$set': {'current_price': price_tag.text}})
 
         elif order_URL.startswith("https://www.balaan"):
             price_tag = soup.select_one(
                 '#price')
-            print('balaan 탐색중!', price_tag.text.lstrip('￦'), price)
-            if price_tag.text.lstrip('￦') <= price:
+            print('balaan 탐색중!', price_tag.text.strip().lstrip('￦'), price)
+            if price_tag.text.strip().lstrip('￦') <= price:
                 send_email(mail_address)
                 db.infos.update_one({'model': order_URL}, {'$set': {'flag': 1}})
             else:
-                db.infos.update_one({'model': order_URL}, {'$set': {'current_price': price_tag.text.lstrip('￦')}})
+                db.infos.update_one({'model': order_URL}, {'$set': {'current_price': price_tag.text.strip()}})
 
 
 
